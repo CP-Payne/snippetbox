028957e4025feb1c1c1064a7cc58d137adb9d482
CP Payne <charlethical1@gmail.com>
2024-03-18 16:18:39 +0200

    Added database connection pool. Created crud operations for snippet table in database

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -52,11 +52,22 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// User r.Method to check whether the request is post or not
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		// http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+
+	// Dummy data
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/CP-Payne/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	// The below will return a pointer to the address value and not the value itself
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Define a new command line flag for the MYSQL DSN string.
+	dsn := flag.String("dsn", "web:CHANGETHISPASSWORD@/snippetbox?parseTime=true", "MySQL data source name")
 
 	// If parse is not called then the addr flag will always be its default value
 	flag.Parse()
@@ -22,9 +31,18 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	// Created separate function for creating conenction pool to keep main tidy.
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	// Set a new http.server struct in order to make use of our new errorLog logger
@@ -38,6 +56,18 @@ func main() {
 
 	infoLog.Printf("Starting server on %s", *addr)
 	// err := http.ListenAndServe(*addr, mux)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	// Use ping to create a connection and check for errors
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
