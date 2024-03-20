1a4af399e34fb471e7b07cdb7e24e25bbb4074e3
CP Payne <charlethical1@gmail.com>
2024-03-20 19:32:21 +0200

    Get function in model shortened

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -39,10 +39,14 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
     WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-	row := m.DB.QueryRow(stmt, id)
-
 	s := &Snippet{}
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	// row := m.DB.QueryRow(stmt, id)
+	// err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	// Errors from DB.QueryRow are deferred until Scan is called.
+	// We can therefore shorten the above code like so
+	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrNoRecord
